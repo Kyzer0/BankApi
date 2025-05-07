002785e3347a687b415d92e2230574719e30e40f
ProgrammerNoob <sungshigun@gmail.com>
2025-05-08 07:31:32 +0800

    fix response

--- CostumeResponse/ApiResponse.cs
+++ CostumeResponse/ApiResponse.cs
@@ -6,6 +6,7 @@
     public class ApiResponse<T>
     {
         public bool isSuccess { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
         public T? Data { get; set; }
         public List<string> Errors { get; set; } = new();
