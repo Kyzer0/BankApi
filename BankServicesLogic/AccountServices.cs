4639dbcb0e2273dd852545675f641a0a43be2318
ProgrammerNoob <sungshigun@gmail.com>
2025-05-07 16:07:45 +0800

    feat: Adding AccountServices and IAccount Interface

--- BankServicesLogic/AccountServices.cs
+++ BankServicesLogic/AccountServices.cs
@@ -1,7 +1,19 @@
-﻿namespace BankServicesLogic
-{
-    public class AccountServices
-    {
+﻿using BankServicesLogic.InterfaceServices;
+using ModelDto.AccountDto;
+using Models;
 
+namespace BankServicesLogic
+{
+    public class AccountServices : IAccountServices
+    {
+        private readonly List<Account> _account;
+        public AccountServices()
+        {
+            _account = new List<Account>();
+        }
+        public AccountResponse AddAccount(AccountRequest addAccount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BankServicesLogic/InterfaceServices/IAccountServices.cs
+++ BankServicesLogic/InterfaceServices/IAccountServices.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using ModelDto.AccountDto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BankServicesLogic.InterfaceServices
 {
     internal interface IAccountServices
     {
-
+        /// <summary>
+        /// Add Account for users
+        /// </summary>
+        /// <param name="addAccount"></param>
+        /// <returns></returns>
+        AccountResponse AddAccount(AccountRequest addAccount);
     }
 }
