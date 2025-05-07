1f3957e4594f28db8299280a23975aa45b9ce9da
ProgrammerNoob <sungshigun@gmail.com>
2025-05-07 14:48:28 +0800

    Add: Adding Account Request Dto

--- ModelDto/AccountRequest.cs
+++ ModelDto/AccountRequest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,25 @@ namespace ModelDto
     /// </summary>
     public class AccountRequest
     {
-       
+        [Required(ErrorMessage = "Name is Required")]
+        public string CostumerName { get; set; } = string.Empty;
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is Required")]
+        public string CostumerEmail { get; set; } = string.Empty;
+        public string? Gender { get; set; }
+        public DateTime? BirthDay { get; set; }
+
+
+        public Account MapAccountRequest()
+        {
+            return new Account
+            {
+                CostumerName = this.CostumerName,
+                CostumerEmail = this.CostumerEmail,
+                Gender = (Gender != null)? Gender.ToString() : "Invalid Null",
+                BirthDay = this.BirthDay,
+            };
+
+        }
     }
 }
