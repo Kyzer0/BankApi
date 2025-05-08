228d9dba6727e90981d6db71f3d807beb7860e0c
ProgrammerNoob <sungshigun@gmail.com>
2025-05-08 08:28:39 +0800

    ADD: Unit testing for Add Account

--- TestBankingFunctionality/Helpers/AsserApiHelpers.cs
+++ TestBankingFunctionality/Helpers/AsserApiHelpers.cs
@@ -13,17 +13,28 @@ namespace TestBankingFunctionality.Helpers
     public class AsserApiHelpers
     {
         /// <summary>
-        /// Reusable Result for all testing
+        /// Reusable Result for all testing if Invalid
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="result"></param>
         /// <param name="expectedMessage"></param>
         /// <param name="expectedError"></param>
-        public void AsserApiError<T>(ApiResponse<T> result, string expectedMessage, string expectedError)
+        public static void AsserApiError<T>(ApiResponse<T> result, string expectedMessage, string expectedError)
         {
             Assert.False(result.isSuccess);
             Assert.Equal(expectedMessage, result.Message);
             Assert.Contains(expectedError, result.Errors);
+        }
+        /// <summary>
+        /// Return Valid Request Result 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="expectedMessage"></param>
+        public static void AsserApiSuccess<T>(ApiResponse<T> result, string expectedMessage)
+        {
+            Assert.True(result.isSuccess);
+            Assert.Equal(expectedMessage, result.Message);
         }
     }
 }
--- TestBankingFunctionality/UnitTest1.cs
+++ TestBankingFunctionality/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using BankServicesLogic;
 using BankServicesLogic.InterfaceServices;
 using ModelDto.AccountDto;
+using TestBankingFunctionality.Helpers;
+using Models.Enums;
+
 namespace TestBankingFunctionality
 {
     public class UnitTest1
@@ -10,14 +13,57 @@ namespace TestBankingFunctionality
         {
             _account = new AccountServices();
         }
-
+        #region
         [Fact]
-        public void AddAccount_CheckPersonRequest_ifNull()
+        public void AddAccount_ShouldReturnError_WhenNull()
         {
-            AccountRequest? account = null;
+            var result = _account.AddAccount(null);
 
-            Assert.Null(account);
-
+            AsserApiHelpers.AsserApiError(result, "Account Cannot be null", "Null request received");
         }
+        [Fact]
+        public void AddAccount_EmptyAccountName_CheckWhenEmpty()
+        {
+            var addAccount = new AccountRequest()
+            {
+                CostumerName = "",
+                CostumerEmail = "dsadas@gmail.com",
+                BirthDay = DateTime.Parse("2022-11-11"),
+                Gender = GenderOptions.Male
+            };
+            var added_account = _account.AddAccount(addAccount);
+
+            AsserApiHelpers.AsserApiError(added_account, "Name is Cannot be Empty", "Name is Required");
+        }
+        [Fact]
+        public void AddAccount_EmptyAccountEmail_CheckWhenEmpty()
+        {
+            var addAccount = new AccountRequest()
+            {
+                CostumerName = "sadasdasd",
+                CostumerEmail = "",
+                BirthDay = DateTime.Parse("2022-11-11"),
+                Gender = GenderOptions.Male
+            };
+            var added_account = _account.AddAccount(addAccount);
+
+            AsserApiHelpers.AsserApiError(added_account, "Email is Cannot be Empty", "Email is Required");
+        }
+        [Fact]
+        public void Add_ValidAccount_ReturnAssertResult()
+        {
+            var addAccount = new AccountRequest()
+            {
+                CostumerName = "sadasdasd",
+                CostumerEmail = "sadasda@gmail.com",
+                BirthDay = DateTime.Parse("2022-11-11"),
+                Gender = GenderOptions.Male
+            };
+            var added_account = _account.AddAccount(addAccount);
+
+            AsserApiHelpers.AsserApiSuccess(added_account, "Successfully Added");
+        }
+
+        #endregion
     }
 }
