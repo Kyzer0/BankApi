f7aad74432aba6a2053ef3382f6e9426abf0db94
ProgrammerNoob <sungshigun@gmail.com>
2025-05-09 06:58:09 +0800

    feat: AddAccount Service

--- BankServicesLogic/AccountServices.cs
+++ BankServicesLogic/AccountServices.cs
@@ -1,4 +1,5 @@
-﻿using BankServicesLogic.InterfaceServices;
+﻿using BankServicesLogic.Helpers;
+using BankServicesLogic.InterfaceServices;
 using CostumeResponse;
 using ModelDto.AccountDto;
 using Models;
@@ -12,9 +13,50 @@ namespace BankServicesLogic
         {
             _account = new List<Account>();
         }
+        /// <summary>
+        /// Add Account for User
+        /// </summary>
+        /// <param name="addAccount"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public ApiResponse<AccountResponse> AddAccount(AccountRequest addAccount)
-        {
-            throw new NotImplementedException();
+        {     
+            var response = new ApiResponse<AccountResponse>();
+
+            if(addAccount == null)
+            {
+                response.isSuccess = false; 
+                response.Message = "Account Cannot be null";
+                response.Errors.Add("Null request received");
+                return response;
+            }
+            var validationError = ModelValidation.ValidateRequest(addAccount);
+
+            if (validationError.Any())
+            {
+                response.isSuccess = false;
+                response.Message = "Validation Failed";
+                response.Errors.AddRange(validationError);
+                return response;
+            }
+            bool emailExist = _account.Any(x => x.CostumerEmail == addAccount.CostumerEmail);
+
+            if (emailExist)
+            {
+                response.isSuccess = false;
+                response.Message = "Duplicate Email";
+                response.Errors.Add("An Account with this Email  Already Exist");
+                return response;
+            }
+            var mapAccount = addAccount.MapAccountModel();
+            _account.Add(mapAccount);
+
+            response.isSuccess = true;
+            response.Message = "Successfully Added";
+            response.Data = new AccountResponse();
+
+            return response;
+
         }
     }
 }
---
+++ BankServicesLogic/Helpers/ModelValidation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankServicesLogic.Helpers
+{
+    internal class ModelValidation
+    {
+        internal static List<string> ValidateRequest<T>(T items)
+        {
+            ValidationContext validationContext = new ValidationContext(items);
+
+            List<ValidationResult> validationResults = new();
+            Validator.TryValidateObject(items, validationContext, validationResults, true);
+
+            return validationResults.Select(x => x.ErrorMessage).ToList();
+        }
+        internal static void ValidateId(Guid? Id)
+        {
+            if(Id == null)
+                throw new ArgumentNullException(nameof(Id), "Id cannot be null");
+            if (Id == Guid.Empty)
+                throw new ArgumentException("Id cannot be empty", nameof(Id));
+        }
+        
+    }
+}
